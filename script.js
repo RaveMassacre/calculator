cbf2fe42364fba8057508a27b21c408201d0cace
Ilya Nosov <ravemassacre22@gmail.com>
2021-07-30 18:43:21 +0500

    feat(script.js):added values ​​and their output

---
+++ script.js
@@ -0,0 +1,22 @@
+let money = 50000;
+console.log(typeof money);
+
+let income = 'freelance';
+console.log(typeof income);
+
+let addExpenses = 'Internet, Taxi, Communal';
+console.log('Длина строки:' + ' ' + addExpenses.length);
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+let deposit = true;
+console.log(typeof deposit);
+
+let mission = 1000000;
+console.log('Цель заработать' + ' ' + mission + ' ' + 'рублей');
+
+let period = 6;
+console.log('Период равен' + ' ' + period + ' ' + 'месяцев');
+
+let budgetDay = (money / 30);
+console.log('Дневной бюджет составляет:' + ' ' + budgetDay);
