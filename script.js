e0c6d8bba480ceab025a6d8de09130728bc323e5
Ilya Nosov <ravemassacre22@gmail.com>
2021-07-31 16:14:12 +0500

    fix(script.js): fixed code structure

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-let money = 50000;
+const money = 50000,
+  income = 'freelance',
+  addExpenses = 'Internet, Taxi, Communal',
+  deposit = true,
+  mission = 1000000,
+  period = 6,
+  budgetDay = (money / 30);
+
 console.log(typeof money);
-
-let income = 'freelance';
 console.log(typeof income);
-
-let addExpenses = 'Internet, Taxi, Communal';
 console.log('Длина строки:' + ' ' + addExpenses.length);
 console.log(addExpenses.toLowerCase());
 console.log(addExpenses.split(', '));
-
-let deposit = true;
 console.log(typeof deposit);
-
-let mission = 1000000;
 console.log('Цель заработать' + ' ' + mission + ' ' + 'рублей');
-
-let period = 6;
 console.log('Период равен' + ' ' + period + ' ' + 'месяцев');
-
-let budgetDay = (money / 30);
 console.log('Дневной бюджет составляет:' + ' ' + budgetDay);
