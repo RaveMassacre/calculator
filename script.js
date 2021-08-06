76e8b8b4e4b291fd909f3c0af688321c97e1bf8a
Ilya Nosov <ravemassacre22@gmail.com>
2021-08-07 00:53:52 +0500

    feat(script.js): loops added to functions

--- script.js
+++ script.js
@@ -1,29 +1,63 @@
 'use strict';
-
-const income = 'freelance';
-const mission = +prompt(`Ваша цель в рублях: `),
-  money = +prompt(`Ваш месячный доход?`),
-  addExpenses = prompt(`Перечислите возможные расходы за рассчитываемый период через запятую`),
-  deposit = confirm(`Есть ли у вас депозит в банке?`),
-  expenses1 = prompt(`Введите обязательную статью расходов?`),
-  amount1 = +prompt(`Во сколько это обойдется?`),
-  expenses2 = prompt(`Введите обязательную статью расходов?`),
-  amount2 = +prompt(`Во сколько это обойдется?`);
-
-//Задание №4:
-
-const getExpensesMonth = function () {
-  return amount1 + amount2;
+let isNumber = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
+
+const income = 'freelance';
+let money,
+  mission = +prompt(`Ваша цель в рублях: `),
+  addExpenses = prompt(`Перечислите возможные расходы за рассчитываемый период через запятую`),
+  deposit = confirm(`Есть ли у вас депозит в банке?`);
+
+//Задание №5:
+//Переписать функцию start циклом do while:
+let start = function () {
+  money = prompt(`Ваш месячный доход?`);
+  do {
+    money = prompt(`Ваш месячный доход?`);
+  } while (!isNumber(money));
+
+};
+start();
+
+//Добавить проверку что введённые данные являются числом
+//Которые мы получаем на вопрос 'Во сколько это обойдется?’ в функцииgetExpensesMonth:
+
+let expenses = [];
+const getExpensesMonth = function () {
+  let sum = 0;
+  for (let i = 0; i < 2; i++) {
+    if (i === 0) {
+      expenses[i] = prompt(`Введите обязательную статью расходов?`);
+    }
+    sum += prompt(`Во сколько это обойдется?`);
+    do {
+      sum += prompt(`Во сколько это обойдется?`);
+    }
+    while (!isNumber(sum));
+
+  }
+  return sum;
+};
+
+let expensesAmount = getExpensesMonth();
+
 const getAccumulatedMonth = function () {
-  return money - getExpensesMonth();
+  return money - expensesAmount;
 };
 
 const accumulatedMonth = getAccumulatedMonth();
 
+//Если getTargetMonth возвращает нам отрицательное значение,
+//то вместо “Цель будет достигнута” необходимо выводить “Цель не будет достигнута:
 const getTargetMonth = function () {
-  return Math.ceil(mission / accumulatedMonth);
+  let result = Math.ceil(mission / accumulatedMonth);
+  if (result > 0) {
+    console.log(`Цель будет достигнута за` + result + `месяцев`);
+  } else {
+    console.log(`К сожалению, цель не будет достигнута`);
+  }
 };
 
 const budgetDay = accumulatedMonth / 30;
@@ -56,7 +90,7 @@ const getStatusIncome = function () {
   }
 };
 
-console.log(`Расходы за месяц: `, getExpensesMonth());
+console.log(`Расходы за месяц: `, expensesAmount);
 console.log(`Возможные расходы: `, addExpenses.toLocaleLowerCase().split(', '));
 console.log(`Срок достижения цели: `, getTargetMonth());
 console.log(`Бюджет на день: `, budgetDay);
